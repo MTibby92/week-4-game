c2abe476956b45ffc983d55c7da84c655ad6ffef
MTibby92 <MTibby92@gmail.com>
2016-08-16 17:00:25 -0500

    added the character name and hp to the html page, corrected the js file to account for the div that wraps the 2 paragraphs and image, adjusted the css a little

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -86,26 +86,32 @@ function selectEnemy(input){
 	$('#'+input).appendTo('#defender')
 }
 
-var yourChar=true;
-
 
 $(document).ready(function(){
+	$('#obiWanKenobe-hp').html(obiWanKenobe.hp)
+	$('#lukeSkywalker-hp').html(lukeSkywalker.hp)
+	$('#darthSidious-hp').html(darthSidious.hp)
+	$('#darthMaul-hp').html(darthMaul.hp)
+
 	$('img').on('click', function(event){
-		input = event.target.id
+		console.log($(event.target).parent().attr('id'))
+		input = $(event.target).parent().attr('id')
+		console.log(input)
 		if (obiWanKenobe.enemy == true && lukeSkywalker.enemy == true && darthSidious.enemy == true && darthMaul.enemy == true) {
 			selectCharacter(input)
 		}else if ((obiWanKenobe.enemy == false || lukeSkywalker.enemy == false || darthSidious.enemy == false || darthMaul.enemy == false) && $('#defender').has('img').length == 0) {
-			selectEnemy(event.target.id)
+			console.log('in the select enemy block')
+			selectEnemy(input)
 		}
 	})
 	$('button').on('click', function(event){
 		inputB = event.target.value
-		//console.log('button clicked')
-		//console.log(inputB)
-		//console.log(event.target.id)
+		console.log('button clicked')
+		console.log(inputB)
+		console.log(event.target.id)
 		if (inputB == 'attack'){
-			var good = eval($('#your-character img').first().attr('id'))
-			var bad = eval($('#defender img').first().attr('id'))
+			var good = eval($('#your-character div').first().attr('id'))
+			var bad = eval($('#defender div').first().attr('id'))
 			if (good.hp <= 0) {
 				$('#defender').append('<p>You have been defeated...GAME OVER</p>')
 				$('#defender').append('<button id="resetButton" value="reset">Reset</button>')
@@ -115,6 +121,10 @@ $(document).ready(function(){
 			}else {
 				good.attackEnemy(bad)
 				bad.counterPlayer(good)
+
+				$('#' + good.identity + '-hp').html(good.hp)
+				$('#' + bad.identity + '-hp').html(bad.hp)
+
 				console.log(good.hp)
 				console.log(good.attack)
 				console.log(bad.hp)
